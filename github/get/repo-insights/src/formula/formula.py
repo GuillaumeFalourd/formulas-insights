7c8b645e9eaaf6c846941d8a6cb34f2a7b2222f6
Guillaume Falourd <guillaume.falourd@zup.com.br>
2020-12-02 18:06:55 -0300

    Update formula.py

--- github/get/repo-insights/src/formula/formula.py
+++ github/get/repo-insights/src/formula/formula.py
@@ -153,7 +153,7 @@ def get_email(events, login, name):
                         email = author["email"]
                         found_email = True
                     if not found_email and name.split()[0].lower() in author["name"] and "github" not in author["email"]:
-                        email = author["email"] + " *" # The * represente an email that is related but not necessary from this user account.
+                        email = author["email"] + " *" # The * represents an email that is related but not necessary from this user account.
     return email
 
 def get_name(events, login):
