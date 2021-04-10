38c675a267e2b441142db3629c63daae201b0cc5
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-04-10 13:20:15 -0300

    update formula inputs
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- linkedin/search/jobs/src/formula/formula.py
+++ linkedin/search/jobs/src/formula/formula.py
@@ -12,30 +12,31 @@ from sendgrid.helpers.mail import (Mail, Attachment, FileContent, FileName, File
 
 from bs4 import BeautifulSoup as soup
 
-def run(city, profession, send_email, email_receiver):
+def run(city, profession, send_email, email_receiver, sendgrid_api_key, sendgrid_email_sender):
     colorama.init()
 
     if [city, profession] is not None:
         try:
             response = requests.get(
-                    f'https://www.linkedin.com/jobs/search?keywords={profession}&location={city}&geoId=&trk=homepage-jobseeker_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0'
+                    f'https://www.linkedin.com/jobs/search/?keywords={profession}&location={city}'
+#                    f'https://www.linkedin.com/jobs/search?keywords={profession}&location={city}&geoId=&trk=homepage-jobseeker_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0'
                 )
             response.raise_for_status()
             page_soup = soup(response.text, 'html.parser')
-            return extract_job_links(profession, city, page_soup, send_email, email_receiver)
-        
+
+            # Extract Job Links
+            job_links = []
+            for res_card in page_soup.findAll("li", {"class": "result-card"})[0:]:
+                for links in res_card.findAll('a', {'class': 'result-card__full-card-link'})[0:]:
+                    job_links.append(links['href'])
+
+            return scrape_datas(profession, city, job_links, send_email, email_receiver, sendgrid_api_key, sendgrid_email_sender)
+
         except requests.HTTPError as err:
             print(colorama.Fore.RED, f'❌ Something went wrong! {err}', colorama.Style.RESET_ALL)
 
-def extract_job_links(profession, city, cursor, send_email, email_receiver):
-    job_links = []
-    for res_card in cursor.findAll("li", {"class": "result-card"})[0:]:
-        for links in res_card.findAll('a', {'class': 'result-card__full-card-link'})[0:]:
-            job_links.append(links['href'])
 
-    return scrape_write(profession, city, job_links, send_email, email_receiver)
-
-def scrape_write(profession, city, links, send_email, email_receiver):
+def scrape_datas(profession, city, links, send_email, email_receiver, sendgrid_api_key, sendgrid_email_sender):
     try:
         if '-' in profession:
             formatting = [x.capitalize() for x in profession.split('-')]
@@ -49,7 +50,7 @@ def scrape_write(profession, city, links, send_email, email_receiver):
         current_date_format = current_date.strftime("%m-%d-%Y-%Hh%M")
         current_date_format_string = str(current_date_format)
         csv_filename = f'{my_job}_{city}_{current_date_format_string}.csv'
-        
+
         with open(csv_filename, 'w', encoding='utf-8') as f:
             headers = ['Source', 'Organization', 'Job Title', 'Location', 'Posted', 'Applicants Hired', 'Seniority Level', 'Employment Type', 'Job Function', 'Industry']
             write = csv.writer(f, dialect='excel')
@@ -63,7 +64,7 @@ def scrape_write(profession, city, links, send_email, email_receiver):
                 job_soup = soup(page_req.text, 'html.parser')
                 my_data = [job_link]
 
-                # Topcard scraping                
+                # Topcard scraping
                 for content in job_soup.findAll('div', {'class': 'topcard__content-left'})[0:]:
                     # Scraping Organization Names
                     orgs = {'Default-Org': [org.text for org in content.findAll('a', {'class': 'topcard__org-name-link topcard__flavor--black-link'})],
@@ -80,9 +81,9 @@ def scrape_write(profession, city, links, send_email, email_receiver):
                     for title in content.findAll('h1', {'class': 'topcard__title'})[0:]:
                         print(colorama.Fore.GREEN,
                                 f'📌 {title.text}',
-                                colorama.Style.RESET_ALL, 
-                                colorama.Fore.YELLOW, 
-                                f'- {org}', 
+                                colorama.Style.RESET_ALL,
+                                colorama.Fore.YELLOW,
+                                f'- {org}',
                                 colorama.Style.RESET_ALL)
                         my_data.append(title.text.replace(',', '.'))
 
@@ -116,32 +117,34 @@ def scrape_write(profession, city, links, send_email, email_receiver):
                     my_data.append(criteria.text)
 
                 write.writerows([my_data])
-            
+
             print(colorama.Fore.YELLOW, f'\n🕵️  Written all information in: {csv_filename}', colorama.Style.RESET_ALL)
-        
+
         check_file(csv_filename)
-        
+
         if send_email == "yes":
-            """ 
-            To use SENDGRID, 
-            inform RIT_SENDGRID_API_KEY and RIT_SENDGRID_EMAIL_SENDER 
+            """
+            To use SENDGRID,
+            inform RIT_SENDGRID_API_KEY and RIT_SENDGRID_EMAIL_SENDER
             as local variables.
             """
-            if os.environ.get('RIT_SENDGRID_API_KEY') is not None:
+            if sendgrid_api_key is not None:
                 print("\n\033[1m🤖 Sending Email...\033[0m")
-                send_mail(csv_filename, my_job, city, email_receiver)
+                send_mail(csv_filename, my_job, city, email_receiver, sendgrid_api_key, sendgrid_email_sender)
             else:
                 print("\n\033[1m🤖 SENDRIG not configured...\033[0m")
                 print("\n\033[1m🤖 If you want to send a message when an error occurs, add RIT_SENDGRID_API_KEY and RIT_SENDGRID_EMAIL_SENDER as local variables.\033[0m")  
-    
+
     except requests.HTTPError as err:
         print(colorama.Fore.RED, f'❌ Something went wrong! {err}', colorama.Style.RESET_ALL)
-        
+
+
 def get_nums(string):
     a_list = string.split()
     for num in a_list:
         if num.isdigit():
             return num
+
 
 def check_file(filename):
     for root, dirs, files in os.walk(f'{os.getcwd()}'):
@@ -151,20 +154,17 @@ def check_file(filename):
                 print(f"\n\033[1m✅ Successfully generated \033[4m{filename}\033[0m\033[1m file!\033[0m")
 
 
-def send_mail(filename, my_job, city, email_receiver):
+def send_mail(filename, my_job, city, email_receiver, sendgrid_api_key, sendgrid_email_sender):
     try:
-        sg = sendgrid.SendGridAPIClient(api_key=os.environ.get("RIT_SENDGRID_API_KEY"))
-        from_email = os.environ.get("RIT_SENDGRID_EMAIL_SENDER")
-        to_email = email_receiver.replace(',', '')
-        subject = f"Linkedin: {my_job} jobs in {city} (Weekly)."
-        content = f"Automated report for {my_job} jobs in {city} generated on {datetime.datetime.now()}."
+        sg = sendgrid.SendGridAPIClient(api_key=sendgrid_api_key)
+
         message = Mail(
-            from_email = from_email,
-            to_emails = to_email,
-            subject = subject,
-            html_content = content
+            from_email = sendgrid_email_sender,
+            to_emails = email_receiver.replace(',', ''),
+            subject = f"LinkedIn: {my_job} jobs in {city} (Weekly).",
+            html_content = f"Automated report for {my_job} jobs in {city} generated on {datetime.datetime.now()}."
         )
-                                
+
         with open(filename, 'rb') as f:
             data = f.read()
             f.close()
@@ -176,11 +176,11 @@ def send_mail(filename, my_job, city, email_receiver):
         attachment.disposition = Disposition('attachment')
         attachment.content_id = ContentId('')
         message.attachment = attachment
-        
+
         response = sg.client.mail.send.post(request_body=message.get())
 
-        print(f"\n\033[1m📩 Email sent successfully to {to_email}\033[0m")
-    
+        print(f"\n\033[1m📩 Email sent successfully to {email_receiver}\033[0m")
+
     except Exception as e:
         print("Error:", e)
         print("\n\033[1m❌ An error occurred while trying to send the email!\033[0m")
--- linkedin/search/jobs/src/main.py
+++ linkedin/search/jobs/src/main.py
@@ -7,5 +7,7 @@ city = os.environ.get("RIT_JOB_CITY")
 profession = os.environ.get("RIT_JOB_TITLE")
 send_email = os.environ.get("RIT_SEND_EMAIL")
 email_receiver = os.environ.get("RIT_EMAIL_RECEIVER")
+sendgrid_api_key = os.environ.get("RIT_SENDGRID_API_KEY")
+sendgrid_email_sender = os.environ.get("RIT_SENDGRID_EMAIL_SENDER")
 
-formula.run(city, profession, send_email, email_receiver)
+formula.run(city, profession, send_email, email_receiver, sendgrid_api_key, sendgrid_email_sender)
