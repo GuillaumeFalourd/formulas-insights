adf2b0af3466a3a6f7cbca1ad3786549ebddcaad
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-05-04 15:12:32 -0300

    refactor search jobs formula
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- linkedin/search/jobs/src/classes/csv.py
+++ linkedin/search/jobs/src/classes/csv.py
@@ -17,22 +17,16 @@ def filename(job, city):
 
 def generate_file(csv_filename, job, city, links):
     try:
-        if len(links) == 0:
-            print(f"\033[1;36m\n⚠️  Couldn't extract job links list from LinkedIn, try again later!\033[0m")
-        
-        else:
-            print(f'\033[1;33m\n🕵️  There are {len(links)} available {job} jobs in {city.capitalize()}.\n\033[0m')
+        with open(csv_filename, 'w', encoding='utf-8') as f:
+            headers = ['Source', 'Organization', 'Job Title', 'Location', 'Posted', 'Applicants Hired', 'Seniority Level', 'Employment Type', 'Job Function', 'Industry']
+            write = csv.writer(f, dialect='excel')
+            write.writerow(headers)
+            
+            for job_link in links:
+                job_datas = scrap.get_datas(job, city, job_link)
+                write.writerows([job_datas])
 
-            with open(csv_filename, 'w', encoding='utf-8') as f:
-                headers = ['Source', 'Organization', 'Job Title', 'Location', 'Posted', 'Applicants Hired', 'Seniority Level', 'Employment Type', 'Job Function', 'Industry']
-                write = csv.writer(f, dialect='excel')
-                write.writerow(headers)
-                
-                for job_link in links:
-                    job_datas = scrap.get_datas(job, city, job_link)
-                    write.writerows([job_datas])
-
-                print(f'\033[1;33m\n🕵️  Written all information in: {csv_filename}\033[0m')
+            print(f'\033[1;33m\n🕵️  Written all information in: {csv_filename}\033[0m')
                 
     except requests.HTTPError as err:
         print(f'\033[0;31m❌ Something went wrong!\033[0m', err)
